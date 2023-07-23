0563d94bf1488d0b4e870e45d66076cb566478c9
Nareshkumar Rao <629990+naresh97@users.noreply.github.com>
2023-07-23 19:28:58 +0200

    refactor tasks

--- Application/Task.h
+++ Application/Task.h
@@ -16,13 +16,19 @@ namespace Application {
   template<typename TFunction, typename... TArgs>
     requires std::is_invocable_v<TFunction, TArgs &...>
   class Task {
+  public:
     using tuple_type = std::tuple<TArgs &...>;
     using function_type = TFunction;
 
+    enum class TaskState { Uninitialized, Running, Suspended };
+
+  private:
     std::string_view taskName;
     TaskHandle_t taskHandle{nullptr};
+    TaskState taskState{TaskState::Uninitialized};
 
     struct InternalParam {
+      TaskState &state;
       function_type function;
       tuple_type args;
     } internalParam;
@@ -33,15 +39,20 @@ namespace Application {
         return;
       }
       auto &parameter = *static_cast<InternalParam *>(params);
+      parameter.state = TaskState::Running;
       std::apply(parameter.function, parameter.args);
+      parameter.state = TaskState::Uninitialized;
       TaskUtils::dispose();
     };
 
   public:
     constexpr Task(std::string_view taskName, TFunction &&function, TArgs &...args)
         : taskName(taskName),
-          internalParam{function, {args...}} {};
+          internalParam{taskState, function, {args...}} {};
     void run() {
+      if (taskHandle != nullptr && taskState != TaskState::Uninitialized) {
+        vTaskDelete(taskHandle);
+      }
       auto status = xTaskCreate(
               internalTaskHandler,
               taskName.data(),
@@ -54,15 +65,11 @@ namespace Application {
     }
     void suspend() {
       if (taskHandle != nullptr) vTaskSuspend(taskHandle);
+      taskState = TaskState::Suspended;
     }
     void resume() const {
       if (taskHandle != nullptr) vTaskResume(taskHandle);
-    }
-    ~Task() {
-      if (taskHandle != nullptr) {
-        auto state = eTaskGetState(taskHandle);
-        if (state != eDeleted) vTaskDelete(taskHandle);
-      }
+      taskState = TaskState::Running;
     }
   };
 }// namespace Application
