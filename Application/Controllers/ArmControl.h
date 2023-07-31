f1c8f00f426546dae03904f016d85584d045c3b5
Nareshkumar Rao <629990+naresh97@users.noreply.github.com>
2023-07-31 23:16:01 +0200

    refactor ArmControl

--- Application/Controllers/ArmControl.h
+++ Application/Controllers/ArmControl.h
@@ -1,23 +1,30 @@
 #pragma once
 
-#include "ArmData.h"
-#include <type_traits>
+#include "Hardware/ServoController.h"
+#include "InverseKinematics/Positioning.h"
 
 namespace Application::Controllers {
-  auto createServoControls(auto &servoData);
 
-  template<typename TArmData>
+  template<Hardware::IServoController ServoController>
   class ArmControl {
-    TArmData armData;
-
-    using servo_control_type =
-            decltype(createServoControls(std::declval<typename TArmData::servo_data_types &>()));
-    servo_control_type servoControls;
+    Kinematics::Positioning2D::LinkLengthsArray linkLengths;
+    ServoController &servoController;
 
   public:
-    explicit ArmControl(TArmData armData);
-    void positionTo(std::array<double, 3> targetPosition);
-  };
-}// namespace Application::Controllers
+    explicit ArmControl(
+            ServoController &servoController,
+            Kinematics::Positioning2D::LinkLengthsArray linkLengthsArray
+    )
+        : linkLengths(linkLengthsArray),
+          servoController(servoController) {}
 
-#include "../detail/Controllers/ArmControl.h"
+    void positionTo(std::array<double, 3> targetPosition) {
+      using namespace Application::Kinematics::Positioning2D;
+      auto newPosition = positionEffector(linkLengths, targetPosition);
+      convertToToolAngles(newPosition);
+      for (int i = 0; i < newPosition.size(); ++i)
+        servoController.setTarget(i, newPosition[i], 360);
+    }
+  };
+
+}// namespace Application::Controllers
