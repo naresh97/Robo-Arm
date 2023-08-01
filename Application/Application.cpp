eef48ea1adf4ddac90e1fcaf4a61b2f8f84d44a1
Nareshkumar Rao <629990+naresh97@users.noreply.github.com>
2023-08-01 23:12:57 +0200

    update demo app

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -12,32 +12,24 @@ void Application_RunApplication() {
   using namespace Application::Kinematics;
   using namespace Application::Controllers;
 
-  auto servo1 = ServoData<ServoTypes::SG90>{PWM::Timer::Timer2, PWM::Channel::Channel2, -5};
+  auto servo1 = ServoData<ServoTypes::DS3225>{PWM::Timer::Timer2, PWM::Channel::Channel2, 0};
   auto servo2 = ServoData<ServoTypes::SG90>{PWM::Timer::Timer2, PWM::Channel::Channel1, 0, true};
   auto servo3 = ServoData<ServoTypes::SG90>{PWM::Timer::Timer2, PWM::Channel::Channel3, 5};
-  static auto controller = ServoController{servo1, servo2, servo3};
-  controller.run();
+  static auto servoController = ServoController{servo1, servo2, servo3};
+  servoController.run();
 
   static auto task1 = Task<2000, void (*)()>{[]() {
     TaskUtils::delay(1000);
-    //    controller.setTarget(0, 90, 45);
-    //    controller.setTarget(1, 90, 45);
-    controller.setTarget(2, 45, (90 - 45) / 0.25);
-    //    auto armControl = ArmControl{
-    //            controller,
-    //            {100, 100}
-    //    };
+    auto armControl = ArmControl{
+            servoController,
+            {100, 100}
+    };
 
-    double c{};
-    double i{20};
     while (true) {
-      //      c += i;
-      //      if (c > 100 || c < -100) {
-      //        i = -i;
-      //      }
-      //      auto targetPosition = std::array{c, 150., 3.14 / 2.};
-      //      armControl.positionTo({c, 500., 3.14 / 2.});
-      TaskUtils::delay(100);
+      servoController.setTarget(0, 90, 360);
+      TaskUtils::delay(2000);
+      servoController.setTarget(0, 45, 360);
+      TaskUtils::delay(3000);
     }
   }};
   task1.run();
